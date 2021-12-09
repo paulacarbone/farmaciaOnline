4fda01620951d9fa56f31ec42611e30cb7d387b5
Veronica Balsamo <veritobalsamo@gmail.com>
2021-12-09 00:35:33 -0300

    corrige driver mysql

--- src/main/java/Modelo/Conexion.java
+++ src/main/java/Modelo/Conexion.java
@@ -17,24 +17,17 @@ public class Conexion {
     private final String user = "root";
     private final String password = "";
     private final String url = "jdbc:mysql://localhost:3306/" + base;
-    
+
     private Connection con = null;
-    
-    public Connection getConexion(){
-        try{
-           Class.forName("com.mysql.jbdc.Driver");
-           con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
-           
+
+    public Connection getConexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+
         } catch (Exception e) {
             System.err.println(e);
         }
         return con;
-        
-
-
+    }
 }
-}
-
-    
-
-
