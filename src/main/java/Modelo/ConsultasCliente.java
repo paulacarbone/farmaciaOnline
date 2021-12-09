a6b92f8f0b9406bf42a20623457b207157117979
Veronica Balsamo <veritobalsamo@gmail.com>
2021-12-09 00:45:13 -0300

    agrega parse de fecha de nacimiento

--- src/main/java/Modelo/ConsultasCliente.java
+++ src/main/java/Modelo/ConsultasCliente.java
@@ -6,142 +6,149 @@
 package Modelo;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  *
  * @author Pau
  */
-public class ConsultasCliente extends Conexion{
-    
-    public boolean registrar(Cliente pro){
-    PreparedStatement ps = null;
-    
-    String sql = "INSERT INTO clientes(nombre,apellido,dni,email,telefono,fecha_nacimiento,cobertura_medica) VALUES(?,?,?,?,?,?,?)";
-    Connection con = getConexion();
-    
-    try{
-        ps = con.prepareStatement(sql);
-        ps.setString(1, pro.getNombre());
-        ps.setString(2, pro.getApellido());
-        ps.setInt(3, pro.getDni());
-        ps.setString(4, pro.getEmail());
-        ps.setInt(5, pro.getTelefono());
-        ps.setString(6, pro.getFecha_nacimiento());
-        ps.setString(7, pro.getCobertura_medica());
-        ps.execute();
-        return true;
-        
-    } catch(SQLException e){
-         System.err.println(e);
-         return false;
-    
-    } finally{
-        try{
-            con.close();
-            
-        } catch(SQLException e){
+public class ConsultasCliente extends Conexion {
+
+    public boolean registrar(Cliente pro) {
+        PreparedStatement ps = null;
+
+        String sql = "INSERT INTO clientes(nombre,apellido,dni,email,telefono,fecha_nacimiento,cobertura_medica) VALUES(?,?,?,?,?,?,?)";
+        Connection con = getConexion();
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pro.getNombre());
+            ps.setString(2, pro.getApellido());
+            ps.setInt(3, pro.getDni());
+            ps.setString(4, pro.getEmail());
+            ps.setInt(5, pro.getTelefono());
+            ps.setDate(6, formatearFecha(pro.getFecha_nacimiento()));
+            ps.setString(7, pro.getCobertura_medica());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
             System.err.println(e);
+            return false;
+
+        } finally {
+            try {
+                con.close();
+
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+
         }
-    
     }
-    }
-       public boolean modificar(Cliente pro){
-    PreparedStatement ps = null;
-    
-    String sql = "UPDATE clientes SET nombre=?, apellido=?, dni=?, email=?, telefono=?, fecha_nacimiento=?, cobertura_medica=? WHERE id_cliente=?";
-    Connection con = getConexion();
-    
-    try{
-        ps = con.prepareStatement(sql);
-        ps.setString(1, pro.getNombre());
-        ps.setString(2, pro.getApellido());
-        ps.setInt(3, pro.getDni());
-        ps.setString(4, pro.getEmail());
-        ps.setInt(5, pro.getTelefono());
-        ps.setString(6, pro.getFecha_nacimiento());
-        ps.setString(7, pro.getCobertura_medica());
-        ps.setInt(8, pro.getId_cliente());
-        ps.execute();
-        return true;
-        
-    } catch(SQLException e){
-         System.err.println(e);
-         return false;
-    
-    } finally{
-        try{
-            con.close();
-            
-        } catch(SQLException e){
+
+    public boolean modificar(Cliente pro) {
+        PreparedStatement ps = null;
+
+        String sql = "UPDATE clientes SET nombre=?, apellido=?, dni=?, email=?, telefono=?, fecha_nacimiento=?, cobertura_medica=? WHERE id_cliente=?";
+        Connection con = getConexion();
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pro.getNombre());
+            ps.setString(2, pro.getApellido());
+            ps.setInt(3, pro.getDni());
+            ps.setString(4, pro.getEmail());
+            ps.setInt(5, pro.getTelefono());
+            ps.setDate(6, formatearFecha(pro.getFecha_nacimiento()));
+            ps.setString(7, pro.getCobertura_medica());
+            ps.setInt(8, pro.getId_cliente());
+            ps.execute();
+            return true;
+
+        } catch (SQLException e) {
             System.err.println(e);
+            return false;
+
+        } finally {
+            try {
+                con.close();
+
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+
         }
-    
     }
-    } 
-       
-              public boolean eliminar(Cliente pro){
-    PreparedStatement ps = null;
-    
-    String sql = "DELETE FROM clientes WHERE id_cliente=?";
-    Connection con = getConexion();
-    
-    try{
-        ps = con.prepareStatement(sql);
-        ps.setInt(1, pro.getId_cliente());
-        ps.execute();
-        return true;
-        
-    } catch(SQLException e){
-         System.err.println(e);
-         return false;
-    
-    } finally{
-        try{
-            con.close();
-            
-        } catch(SQLException e){
+
+    public boolean eliminar(Cliente pro) {
+        PreparedStatement ps = null;
+
+        String sql = "DELETE FROM clientes WHERE id_cliente=?";
+        Connection con = getConexion();
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, pro.getId_cliente());
+            ps.execute();
+            return true;
+
+        } catch (SQLException e) {
             System.err.println(e);
+            return false;
+
+        } finally {
+            try {
+                con.close();
+
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+
         }
-    
     }
-    } 
-              
-    public boolean buscar(Cliente pro){
-    PreparedStatement ps = null;
-    ResultSet rs = null;
-    Connection con = getConexion(); 
-    String sql = "SELECT FROM clientes WHERE dni=?";
-    
-    
-    try{
-        ps = con.prepareStatement(sql);
-        ps.setInt(1, pro.getDni());
-        rs = ps.executeQuery();
-        if(rs.next()){
-            pro.setId_cliente(Integer.parseInt(rs.getString("id_cliente")));
-            pro.setDni(Integer.parseInt(rs.getString("dni")));
-            pro.setNombre(rs.getString("nombre"));
-            pro.setApellido(rs.getString("apellido"));
-            return true;    
-        }
-        
-        return false;
-        
-    } catch(SQLException e){
-         System.err.println(e);
-         return false;
-    
-    } finally{
-        try{
-            con.close();
-            
-        } catch(SQLException e){
+
+    public boolean buscar(Cliente pro) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection con = getConexion();
+        String sql = "SELECT FROM clientes WHERE dni=?";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, pro.getDni());
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                pro.setId_cliente(Integer.parseInt(rs.getString("id_cliente")));
+                pro.setDni(Integer.parseInt(rs.getString("dni")));
+                pro.setNombre(rs.getString("nombre"));
+                pro.setApellido(rs.getString("apellido"));
+                return true;
+            }
+
+            return false;
+
+        } catch (SQLException e) {
             System.err.println(e);
+            return false;
+
+        } finally {
+            try {
+                con.close();
+
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+
         }
-    
     }
-    } 
+
+    private Date formatearFecha(String fechaString) throws ParseException {
+        java.util.Date fechaNacimiento = new SimpleDateFormat("dd/MM/yyyy").parse(fechaString);
+        return new Date(fechaNacimiento.getTime());
+    }
 }
